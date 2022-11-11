83b0ebeb855ac5bdc2cfcd9fa2967402be26ed75
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-11-11 17:16:35 +0100

    Create Validators folder + add CurvePointValidator.cs class with validation rules for a curvePoint

---
+++ Validators/CurvePointValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using WebApi3.Domain;
+
+namespace WebApi3.Validators
+{
+    public class CurvePointValidator : AbstractValidator<CurvePoint>
+    {
+        public CurvePointValidator()
+        {
+            RuleFor(c => c.CurveId).NotEmpty();
+            RuleFor(c => c.Term).NotEmpty();
+            RuleFor(c => c.Value).NotEmpty();
+            RuleFor(c => c.CurveId).GetType().Equals(typeof(int));
+            RuleFor(c => c.Term).GetType().Equals(typeof(decimal));
+            RuleFor(c => c.Value).GetType().Equals(typeof(decimal));
+        }
+    }
+}

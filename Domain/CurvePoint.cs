f92a3e3cf6d125c95df421d037c3a03b267ca28e
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-11-11 16:07:52 +0100

    fill entiy model classes with properties in Domain Folder

--- Domain/CurvePoint.cs
+++ Domain/CurvePoint.cs
@@ -1,7 +1,15 @@
-namespace Dot.Net.WebApi.Domain
+using System;
+
+namespace WebApi3.Domain
 {
     public class CurvePoint
     {
         // TODO: Map columns in data table CURVEPOINT with corresponding fields
+        public int Id { get; set; }
+        public int CurveId { get; set; }
+        public DateTime AsOfDate { get; set; }
+        public decimal Term { get; set; }
+        public decimal Value { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
--- Domain/Rating.cs
+++ Domain/Rating.cs
@@ -1,7 +1,12 @@
-namespace Dot.Net.WebApi.Controllers.Domain
+namespace WebApi3.Domain
 {
     public class Rating
     {
         // TODO: Map columns in data table RATING with corresponding fields
+        public int Id { get; set; }
+        public string MoodysRating { get; set; }
+        public string SandPRating { get; set; }
+        public string FitchRating { get; set; }
+        public int OrderNumber { get; set; }
     }
 }

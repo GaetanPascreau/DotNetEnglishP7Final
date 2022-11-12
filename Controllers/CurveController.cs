cda905cd68dc2f1be9f7c0fc7a6746e3d8a2f293
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-11-12 17:17:43 +0100

    complete CurvePointService and CurvePointController

--- Controllers/CurveController.cs
+++ Controllers/CurveController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApi3.Domain;
+using WebApi3.Domain.DTO;
+using WebApi3.Services;
 using WebApi3.Validators;
 
 namespace Dot.Net.WebApi.Controllers
@@ -8,11 +11,35 @@ namespace Dot.Net.WebApi.Controllers
     public class CurveController : Controller
     {
         // TODO: Inject Curve Point service
+        private readonly ICurvePointService _curvePointService;
+
+        public CurveController(ICurvePointService curvePointService)
+        {
+            _curvePointService = curvePointService;
+        }
 
         [HttpGet("/curvePoint/list")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Home()
         {
-            return View("curvePoint/list");
+            var curvePoints = _curvePointService.GetCurvePoint();
+            //if (curvePoints is empty )
+            //{ return NotFound(); }
+            return Ok(curvePoints.Result);
+        }
+
+        [HttpGet("/curvePoint/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetSpecificCurvePoint(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var curvePoint = _curvePointService.GetCurvePointById(id);
+            return Ok(curvePoint.Result);
         }
 
         /// <summary>
@@ -32,33 +59,58 @@ namespace Dot.Net.WebApi.Controllers
             return BadRequest(result.Errors);
         }
 
-        [HttpGet("/curvePoint/add")]
-        public IActionResult Validate([FromBody]CurvePoint curvePoint)
+        [HttpPut("/curvePoint/update/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateCurvePoint(int Id, CurvePointDTO curvePointDTO)
         {
-            // TODO: check data valid and save to db, after saving return bid list
-            return View("curvePoint/add"    );
+            if (!_curvePointService.UpdateCurvePoint(Id, curvePointDTO))
+            {
+                return BadRequest();
+            }
+            return NoContent();
         }
 
-        [HttpGet("/curvePoint/update/{id}")]
-        public IActionResult ShowUpdateForm(int id)
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteCurvePoint(int Id)
         {
-            // TODO: get CurvePoint by Id and to model then show to the form
-            return View("curvepoint/update");
+            if (Id == 0)
+            {
+                return NotFound();
+            }
+            _curvePointService.DeleteCurvePoint(Id);
+            return Ok();
         }
 
-        [HttpPost("/curvepoint/update/{id}")]
-        public IActionResult UpdateCurvePoint(int id, [FromBody] CurvePoint curvePoint)
-        {
-            // TODO: check required fields, if valid call service to update Curve and return Curve list
-            return Redirect("/curvepoint/list");
-        }
+        //[HttpGet("/curvePoint/add")]
+        //public IActionResult Validate([FromBody]CurvePoint curvePoint)
+        //{
+        //    // TODO: check data valid and save to db, after saving return bid list
+        //    return View("curvePoint/add"    );
+        //}
 
-        [HttpDelete("/curvepoint/{id}")]
-        public IActionResult DeleteBid(int id)
-        {
-            // TODO: Find Curve by Id and delete the Curve, return to Curve list
+        //[HttpGet("/curvePoint/update/{id}")]
+        //public IActionResult ShowUpdateForm(int id)
+        //{
+        //    // TODO: get CurvePoint by Id and to model then show to the form
+        //    return View("curvepoint/update");
+        //}
 
-            return Redirect("/curvePoint/list");
-        }
+        //[HttpPost("/curvepoint/update/{id}")]
+        //public IActionResult UpdateCurvePoint(int id, [FromBody] CurvePoint curvePoint)
+        //{
+        //    // TODO: check required fields, if valid call service to update Curve and return Curve list
+        //    return Redirect("/curvepoint/list");
+        //}
+
+        //[HttpDelete("/curvepoint/{id}")]
+        //public IActionResult DeleteBid(int id)
+        //{
+        //    // TODO: Find Curve by Id and delete the Curve, return to Curve list
+
+        //    return Redirect("/curvePoint/list");
+        //}
     }
 }
---
+++ Services/CurvePointService.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi3.Domain.DTO;
+using WebApi3.Repositories;
+
+namespace WebApi3.Services
+{
+    public class CurvePointService : ICurvePointService
+    {
+        private readonly ICurvePointRepository _curvePointRepository;
+
+        public CurvePointService(ICurvePointRepository curvePointRepository)
+        {
+            _curvePointRepository = curvePointRepository;
+        }
+
+        /// <summary>
+        /// Method that calls the GetCurvePoint() method from CurvePointRepository 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<CurvePointDTO>> GetCurvePoint()
+        {
+            var curvePointsDTO = await _curvePointRepository.GetCurvePoint();
+            return curvePointsDTO;
+        }
+
+        /// <summary>
+        /// Method that calls the GetCurvePointById() method from CurvePointRepository
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<CurvePointDTO> GetCurvePointById(int id)
+        {
+            //ADD validation here => check that the id exists ? or use annotations?
+            if (id == 0)
+            {
+                //use modelErrors like in P3AddNewFunctionalityDotNetCore/ProductService.cs
+            }
+            var curvePointDTO = await _curvePointRepository.GetCurvePointById(id);
+            return curvePointDTO;
+        }
+
+        /// <summary>
+        /// Methods that checks if passed data is valid, then calls the CreateCurvePoint() method from CurvePointRepository
+        /// </summary>
+        /// <param name="curvePointDTO"></param>
+        public bool CreateCurvePoint(CurvePointDTO curvePointDTOtoCreate)
+        {
+            // Validation logic
+            //if (!ValidateCurvePoint(curvePointDTOtoCreate))
+            //{
+            //    return false;
+            //}
+            // Database logic
+            try
+            {
+                _curvePointRepository.CreateCurvePoint(curvePointDTOtoCreate);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Methods that checks if passed data is valid, then calls the UpdateCurvePoint() method from CurvePointRepository
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="curvePointDTOtoUpdate"></param>
+        /// <returns></returns>
+        public bool UpdateCurvePoint(int Id, CurvePointDTO curvePointDTOtoUpdate)
+        {
+            // Validation logic
+            //if (!ValidateCurvePoint(curvePointDTOtoUpdate))
+            //{
+            //    return false;
+            //}
+            // Database logic
+            try
+            {
+                _curvePointRepository.UpdateCurvePoint(Id, curvePointDTOtoUpdate);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Method that calls the DeleteCurvePoint() method from CurvePointRepository 
+        /// </summary>
+        /// <returns></returns>
+        public void DeleteCurvePoint(int Id)
+        {
+            _curvePointRepository.DeleteCurvePoint(Id);
+        }
+    }
+
+    public interface ICurvePointService
+    {
+        Task<IEnumerable<CurvePointDTO>> GetCurvePoint();
+        Task<CurvePointDTO> GetCurvePointById(int id);
+        bool CreateCurvePoint(CurvePointDTO curvePointDTOtoCreate);
+        bool UpdateCurvePoint(int Id, CurvePointDTO curvePointDTOtoUpdate);
+        void DeleteCurvePoint(int Id);
+        //bool ValidateCurvePoint(CurvePointDTO curvePointToValidate);
+    }
+}
